a46c146b03452eb11f30c896ce102eacbfc57ee0
Osmar Neto <osmar.neto23@gmail.com>
2024-01-15 17:27:44 -0300

    Finished header/navbar

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+        'submarine': '#BBCBCB',
+        'baltic-sea': '#302E33',
+        'spice': '#6F3E33',
+        'leather': '#987150',
+      },
+    },
+  },
+  plugins: [],
+}
+
